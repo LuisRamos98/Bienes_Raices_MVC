0883abf1a7b7a912e6e6d4ed8dcf9d0c9e071bdd
Luis Ramos Moncayo <luisandres11061998@gmail.com>
2022-11-19 15:58:30 +0100

    first commit

---
+++ models/ActiveRecord.php
@@ -0,0 +1,204 @@
+<?php
+
+namespace App;
+
+class ActiveRecord {
+
+    protected static $db;
+    protected static $columnasDB = [];
+    protected static $errores = [];
+    protected static $tabla = '';
+
+
+    public function guardar() {
+        if (!is_null($this->id)) {
+            $this->actualizar();
+        } else {
+            $this->crear();
+        }
+    }
+    public function crear(){
+
+        //Sanitizar los atributos
+        $atributos = $this->sanitizarAtributos();
+
+        // Insertar en base de datos
+        $query = "INSERT INTO " . static::$tabla . " (";
+        $query .= join(",",array_keys($atributos)). ") VALUES ('";
+        $query .= join("','",array_values($atributos))."')";
+
+        $resultado = self::$db->query($query);
+
+        //MENSAJE DE RESULTADO O ERROR
+        if($resultado) {
+            // echo "Insertado correctamente";
+            header("Location: /admin?resultado=1");
+        }
+        
+    }
+
+    //Actualizar
+    public function actualizar() {
+        //Sanitizar los atributos
+        $atributos = $this->sanitizarAtributos();
+
+        $valores = [];
+        
+        foreach($atributos as $key => $value) {
+            $valores[] = "{$key}='{$value}'";
+        }
+
+        $query = "UPDATE " . static::$tabla . " SET ";
+        $query .= join(", ",$valores);
+        $query .= "WHERE id = '" . self::$db->escape_string($this->id ) . "' ";
+        $query .=  " LIMIT 1";
+
+        $resultado = self::$db->query($query);
+
+        if($resultado) {
+            // echo "Insertado correctamente";
+            header("Location: /admin?resultado=2");
+        }
+    }
+
+    //Eliminar
+    public function eliminar() {
+        
+        $query = "DELETE FROM " . static::$tabla . " WHERE id = " . self::$db->escape_string($this->id) . " LIMIT 1";
+        // debugear($query);
+        $resultado = self::$db->query($query);
+
+        if($resultado){
+            //ELIMINAMOS LA IMAGEN 
+            $this->borrarImagen();
+            //Redirecionamos
+            header("Location: /admin?resultado=3");
+        }
+    }
+
+    public static function setDB($database) {
+        self::$db = $database;
+    } 
+
+    public function atributos() {
+
+        $atributos = [];
+
+        foreach(static::$columnasDB as $columna) {
+            if($columna==="id") continue;
+            $atributos[$columna] = $this->$columna;
+        }
+        return $atributos;
+    }
+
+    public function sanitizarAtributos() {
+        $atributos = $this->atributos();
+        $sanitizado = [];
+
+        foreach($atributos as $key => $value) {
+            $sanitizado[$key] = self::$db->escape_string($value);
+        }
+
+        return $sanitizado;
+    }
+
+    //Subida de Imagen
+    public function setImagen($imagen){
+        //Eliminamos Archivo anterior
+        if(!is_null($this->id)) {
+            $this->borrarImagen();
+        }
+        if($imagen){
+            $this->imagen = $imagen;
+        }
+        
+    }
+
+    //Elinada de Imagen
+    public function borrarImagen() {
+        //Comprobar si existe el archivo
+        $existeArchivo = file_exists(CARPETA_IMAGENES . $this->imagen);
+
+        if($existeArchivo) {
+            unlink(CARPETA_IMAGENES . $this->imagen);
+        }
+    }
+
+    //RETORNA LOS ERRORES
+    public static function getErrores() {
+        return static::$errores;
+    }
+    
+
+    //Validacion
+    public function validar() {
+        static::$errores = [];
+        return static::$errores;
+    }
+
+
+    //LISTAR TODAS LAS PROPIEDADES
+    public static function all(){
+
+        $query = "SELECT * FROM " . static::$tabla;
+
+        $resultado = self::consultaSQL($query);
+        return $resultado;
+    }
+
+    //Obtener una cantidad determinada de resultados
+    public static function get($cantidad){
+
+        $query = "SELECT * FROM " . static::$tabla . " LIMIT " . $cantidad;
+
+        $resultado = self::consultaSQL($query);
+        return $resultado;
+    }
+
+    public static function find($id) {
+
+        $query = "SELECT * FROM " . static::$tabla . " WHERE id=${id}";
+        $resultado = self::consultaSQL($query);
+
+        return array_shift($resultado);
+    }
+
+    public static function consultaSQL($query) {
+
+        $array = [];
+
+        //HACEMOS LA CONSULTA SQL
+        $resultado = self::$db->query($query);
+        
+        //Iteramos el resultado
+        while($registro = $resultado->fetch_assoc()) {
+            $array[] = static::crearObjeto($registro);
+        }
+
+        //liberamos memoria
+        $resultado->free();
+
+        // debugear($array);
+        return $array;
+    }
+
+    protected static function crearObjeto($registro) {
+        $objeto = new static;
+        
+        foreach($registro as $key => $value) {
+            $objeto->$key = $value;
+        }
+
+        return $objeto;
+    }
+
+    public function sincronizar($args = []) {
+
+        foreach ($args as $key => $value) {
+            if(property_exists($this,$key) && !is_null($value)) {
+                $this->$key = $value;
+            }            
+        }
+    }
+
+}
---
+++ models/Propiedad.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App;
+
+class Propiedad extends ActiveRecord{
+
+    protected static $tabla = 'propiedades';
+    protected static $columnasDB = ['id','titulo',"precio","imagen",'descripcion','habitaciones','wc','estacionamiento','creado','vendedores_id'];
+
+    public $id;
+    public $titulo;
+    public $precio;
+    public $imagen;
+    public $descripcion;
+    public $habitaciones;
+    public $wc;
+    public $estacionamiento;
+    public $creado;
+    public $vendedores_id;
+
+    public function __construct($args = [])
+    {
+        $this->id = $args["id"] ?? null;
+        $this->titulo = $args["titulo"] ?? '';
+        $this->precio = $args["precio"] ?? '';
+        $this->imagen = $args["imagen"] ?? '';
+        $this->descripcion = $args["descripcion"] ?? '';
+        $this->habitaciones = $args["habitaciones"] ?? '';
+        $this->wc = $args["wc"] ?? '';
+        $this->estacionamiento = $args["estacionamiento"] ?? '';
+        $this->creado = date("y/m/d");
+        $this->vendedores_id = $args["vendedores_id"] ?? "";
+    }
+
+    public function validar() {
+        if(!$this->titulo) {
+            self::$errores[] = "Debes ingresar un titulo";
+        }
+
+        if(!$this->precio) {
+            self::$errores[] = "Debes ingresar un precio";
+        }
+
+        if( strlen($this->descripcion) < 50) {
+            self::$errores[] = "Debes ingresar un descripcion y debe ser mayor a 50 caracteres";
+        }
+
+        if(!$this->habitaciones) {
+            self::$errores[] = "El numero de habitacion es obligatoria";
+        }
+
+        if(!$this->wc) {
+            self::$errores[] = "El numero de wc es obligatoria";
+        }
+
+        if(!$this->estacionamiento) {
+            self::$errores[] = "El numero de estacionamientos es obligatoria";
+        }
+
+        if(!$this->vendedores_id) {
+            self::$errores[] = "Ingrese un vendedor ";
+        }
+
+        if(!$this->imagen) {
+            self::$errores[] = "La imagen es obligatoria";
+        }
+
+        return self::$errores;
+    }
+ 
+}
