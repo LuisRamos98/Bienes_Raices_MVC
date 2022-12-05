3a478daee1daa76bf8aadccc2a94ebbca784d2d6
Luis Ramos Moncayo <luisandres11061998@gmail.com>
2022-12-05 18:06:13 +0100

    commit

--- public/index.php
+++ public/index.php
@@ -7,6 +7,12 @@ use MVC\Router;
 $router = new Router();
 
 
+$router->get("/nosotros","funcion_nosotros");
+$router->get("/tienda","funcion_tienda");
+$router->get("/contacto","funcion_contacto");
+$router->get("/admin","funcion_admin");
+
+$router->comprobarRutas();
 
 
 
--- router.php
+++ router.php
@@ -4,8 +4,25 @@ namespace MVC;
 
 class Router {
 
-    public function __construct()
-    {
-        echo "Creando el router...";
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+    public function get($url, $fn) {
+        $this->rutasGET[$url] = $fn;
+    }
+
+    public function comprobarRutas(){
+        $urlActual = $_SERVER["PATH_INFO"];
+        $metodo = $_SERVER["REQUEST_METHOD"];
+        if($metodo === "GET") {
+           $fn = $this->rutasGET[$urlActual] ?? null; 
+        }
+
+        if ($fn) {
+            #si existe la funcion
+            call_user_func($fn,$this);
+        } else {
+            echo "Página No Encontrada";
+        }
     }
 }
