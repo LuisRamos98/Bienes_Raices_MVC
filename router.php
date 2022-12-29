430252c7656a61439d2ad9ed4641b822e5582437
Luis Ramos Moncayo <luisandres11061998@gmail.com>
2022-12-29 17:57:31 +0100

    commit

--- router.php
+++ router.php
@@ -4,6 +4,8 @@ namespace MVC;
 
 class Router {
 
+    public $rutasPrivadas = [];
+
     public $rutasGET = [];
     public $rutasPOST = [];
 
@@ -16,6 +18,14 @@ class Router {
     }
 
     public function comprobarRutas(){
+
+        session_start();
+        $auth = $_SESSION["login"] ?? null;
+
+        //Arreglo de rutas privadas
+        $rutasPrivadas = ["/admin", "/propiedades/crear", "/propiedades/actualizar", "/propiedades/eliminar", 
+                          "/vendedores/crear", "/vendedores/actualizar", "/vendedores/eliminar"];
+
         $urlActual = $_SERVER["PATH_INFO"] ?? $_SERVER["REQUEST_URI"];
         $metodo = $_SERVER["REQUEST_METHOD"];
         if($metodo === "GET") {
@@ -24,9 +34,16 @@ class Router {
             $fn = $this->rutasPOST[$urlActual] ?? null;      
         }
 
+
+        //Proteger Rutas
+        if(in_array($urlActual,$rutasPrivadas) && !$auth) {
+            header("Location: /");
+        }
+
         if ($fn) {
             #si existe la funcion
             call_user_func($fn,$this);
+
         } else {
             echo "Página No Encontrada";
         }
